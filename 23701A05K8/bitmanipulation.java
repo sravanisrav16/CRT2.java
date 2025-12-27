67720974c8384133cae857c10799138ae0fb2b08
sravanisrav16 <sravaninayak1613@gmail.com>
2025-12-27 15:05:20 +0530

    Add files via upload

---
+++ 23701A05K8/bitmanipulation.java
@@ -0,0 +1,12 @@
+class bitmanipulation{
+    public static void main(String[] args) {
+        int a=5; //0101
+        int b=3; //0011
+        System.out.println(a&b);
+        System.out.println(a|b);
+        System.out.println(a^b);
+        System.out.println(~a);
+        System.out.println(10>>1);
+        System.out.println(5<<2);
+    }
+}
